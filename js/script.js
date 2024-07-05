b13798745a4b795414ca40b93546a45d29c513ca
Emiliya Milevskaya <emilia.medvedeva1998@gmail.com>
2024-07-06 01:36:43 +0300

    закоммитила

--- js/script.js
+++ js/script.js
@@ -1,105 +1,4 @@
-// const makeSwiper = () => {
-
-// return new Swiper('.link-slider', {
-
-//      pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-
-//     simulateTouch: true,
-
-//     touchRatio: 1,
-
-//     touchAngle: 45,
-
-//     grabCursor: true,
-
-//     slideToClickedSlide: true,
-
-//     slideToClickedSlide: true,
-
-
-//     keyboard: {
-
-//         enabled: true,
-
-//         onlyInViewport: true,
-
-//         pageUpDown: true,
-//     },
-
-
-//     mousewheel: {
-        
-//         sensitivity: 1,
-
-        
-//         eventsTarget: '.link-slider',
-//     },
-
-//     breakpoints: {
-//         240: {
-//             slidesPerView: 0.8, 
-//         },
-
-//         280: {
-//             slidesPerView: 1,
-//         },
-
-//         320: {
-//             slidesPerView: 1.35,  
-//         },
-
-//         400: {
-//             slidesPerView: 1.7, 
-//         },
-        
-//         480: {
-//             slidesPerView: 2, 
-//         },
-
-//         600: {
-//             slidesPerView: 2.5, 
-//         }, 
-        
-//         755: {
-//             slidesPerView: 3,
-//         },
-
-//         768: {
-//             initialSlide: 0,
-//             spaceBetween: 0,
-//             enabled: false,
-//         },
-
-//     },
-    
-
-//     watchOverflow: true,
-
-//     spaceBetween: 16,
-
-// });
-
-// }
-
-// let swiper = makeSwiper();
-
-// window.addEventListener ('resize', function() {
-//     const width = this.window.innerWidth;
-//     console.log(width);
-//     if(width>767){
-//         if (swiper){
-//             swiper.destroy()
-//             swiper = null;
-//         }
-//     } else {
-//         if (!swiper) swiper = makeSwiper()
-//     }
-// })
-
-const slider = document.querySelector('.link-slider');
+const slider = document.querySelector('.slider');
 
 let mySwiper;
 
@@ -108,7 +7,7 @@ function mobileSlider() {
 
         mySwiper = new Swiper(slider, {
             loop: true,
-            slideClass: 'link-slider__slide',
+            slideClass: 'slider-list__slide',
                  pagination: {
                     el: '.swiper-pagination',
                     clickable: true,
@@ -142,7 +41,7 @@ function mobileSlider() {
                     sensitivity: 1,
 
         
-                    eventsTarget: '.link-slider',
+                    eventsTarget: '.slider',
                 },
 
                 breakpoints: {
@@ -195,7 +94,7 @@ function mobileSlider() {
     if (window.innerWidth > 768) {
         slider.dataset.mobile = 'false';
 
-        if (slider.classList.contains('link-slider-initialized')) {
+        if (slider.classList.contains('slider-initialized')) {
 
             mySwiper.destroy();
         }
--- js/scriptForExplain.js
+++ js/scriptForExplain.js
@@ -1,39 +1,4 @@
-    // const buttons = document.querySelectorAll("button");
-    // buttons.forEach(button => {
-    //    button.addEventListener("click", event => {
-    //       buttons.forEach(b => b.classList.remove("selected"));
-    //       event.target.classList.toggle("selected");
-    //    });
-    // });
-
-    // buttons();
-
-// const brandsButtonOpen = document.querySelector('.brands__button');
-
-// const brandsList = document.querySelector('.brands__brand-list');
-
-// brandsButtonOpen.addEventListener('click', () => {
-//     brandsList.classList.add('brand-list--opened');
-//     brandsButtonOpen.classList.add('brands-button--close');
-//     brandsButtonOpen.classList.remove('brands-button--open');
-// }) 
-
-// const findButtonOpen = document.querySelector('.main__button1');
-
-// function myFunction() {
-
-//     const element = document.getElementById("textOpen");
-//     element.classList.toggle("changeheight");
-// }
-
-
-// findButtonOpen.addEventListener('click', () => {
-
-//     myFunction();
-// })
-
-
-const findButtonOpen = document.querySelector('.main__button1');
+const findButtonOpen = document.querySelector('.brands-main__button1');
 let clicks = 0;
 
 function myFunction() {
@@ -45,7 +10,7 @@ function myFunction() {
     clicks++;
 
     const buttonHidden = document.getElementById('buttonHidden');
-    buttonHidden.classList.toggle("main__button2"); 
+    buttonHidden.classList.toggle("brands-main__button2"); 
 
     if (clicks % 2 === 0) {
         buttonHidden.innerText = 'Показать все';
